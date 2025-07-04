0aa6592a00a6632c5bcc9878f90170456335ec15
Kasarla Vinay <60963959+vinayk009@users.noreply.github.com>
2025-07-04 12:15:32 +0530

    Create Pending_Soothie_Orders.py
    Newly created pending_smoothie_orders app

---
+++ Pending_Soothie_Orders.py
@@ -0,0 +1,46 @@
+#Import python packages
+import streamlit as st
+from snowflake.snowpark.context import get_active_session
+from snowflake.snowpark.functions import col, when_matched
+
+
+
+# write directly to the app
+st.title(f"Orders Devlivered Check :cup_with_straw:")
+st.write("""Check the **Orders**""")
+
+session = get_active_session()
+my_dataframe = session.table("smoothies.public.orders").filter(col("ORDER_FILLED")==0).collect()
+
+if my_dataframe:
+  editable_df = st.data_editor(my_dataframe)
+  submitted = st.button('Submit')
+  if submitted:
+    st.success("Someone clicked the button.")
+    try:
+      og_dataset = session.table("smoothies.public.orders")
+      edited_dataset = session.create_dataframe(editable_df)
+      og_dataset.merge(edited_dataset
+                       , (og_dataset['ORDER_UID'] == edited_dataset['ORDER_UID'])
+                       , [when_mated().update({'ORDER_FILLED': edited_dataset['ORDER_FILLED']})]
+                      )
+      st.success("Order(s) Updated!")
+    except:
+      st.write('Something went wrong.')
+  
+else:
+  st.success('There are no pending orders right now')
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
