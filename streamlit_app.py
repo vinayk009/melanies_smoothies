bc2b51bd97847ce3e6170e9fda6f2b7076b3d41b
Kasarla Vinay <60963959+vinayk009@users.noreply.github.com>
2025-07-04 17:50:45 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,33 +20,21 @@ my_dataframe = session.table(
 pd_df = my_dataframe.to_pandas()
 
 
-ingredient_list = st.multiselect(
-  "Choose upto 5 ingredients: ", 
-  my_dataframe, 
-  max_selections = 5
-)
+ingredient_list = st.multiselect('Choose upto 5 ingredients: ', my_dataframe, max_selections = 5)
 
 time_to_insert = st.button('Submit Order')
-if time_to_inser:
+if time_to_insert:
   if ingredient_list:
+    ingredients_string = ''
+    for fruit_chosen in ingredient_list:
+      ingredients_string += fruit_chosen + ' '
+      search_on = pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+      st.write('The search value for ', fruit_chosen, ' is ', search_on, '.')
+      smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit"+search_on)
+      st.subheader(fruit_chosen + ' Nutrition Information')
+      sf_df = st.dataframe(data = smoothiefroot_response.json(), use_container_width=True)
   
-  ingredients_string = ''
-  
-  for fruit_chosen in ingredient_list:
-    ingredients_string += fruit_chosen + ' '
-
-    search_on = pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
-    st.write('The search value for ', fruit_chosen, ' is ', search_on, '.')
-
-    smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit"+search_on)
-    st.subheader(fruit_chosen + ' Nutrition Information')
-    sf_df = st.dataframe(data = smoothiefroot_response.json(), use_container_width=True)
-  
-  
-        
-  my_insert_stmt = """ insert into smoothies.public.orders
-  (ingredients, name_on_order) 
-  values('""" + ingredients_string + """','""" + name_on_order + """')"""
+  my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order) values('""" + ingredients_string + """','""" + name_on_order + """')"""
   session.sql(my_insert_stmt).collect()
   st.success('Your Smoothie is ordered!')
 
