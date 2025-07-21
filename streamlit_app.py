3c44157eba6eacafc0d9a1f8f1d34741f4cb59da
Kasarla Vinay <60963959+vinayk009@users.noreply.github.com>
2025-07-21 13:05:52 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,8 +46,8 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered,'+name_on_order+'!', icon="✅")
       
-#new section to display smoothiefruit nutrition information
-import requests
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-#st.text(smoothiefroot_response.json())
-sf_df = st.dataframe(data = smoothiefroot_response.json(),use_container_width=True)
+  #new section to display smoothiefruit nutrition information
+  import requests
+  smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+  #st.text(smoothiefroot_response.json())
+  sf_df = st.dataframe(data = smoothiefroot_response.json(),use_container_width=True)
