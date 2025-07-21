286a21110fbdba289fa9d97d63d666d621328b24
Kasarla Vinay <60963959+vinayk009@users.noreply.github.com>
2025-07-21 13:14:29 +0530

    Update Pending_Soothie_Orders.py

--- Pending_Soothie_Orders.py
+++ Pending_Soothie_Orders.py
@@ -1,49 +1,31 @@
-#Import python packages
+# Import python packages
 import streamlit as st
-from snowflake.snowpark import Session
-@st.cache_resource
-def get_session():
-  return Session.builder.configs(st.secrets).create()
+from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col, when_matched
 
+# Write directly to the app
+st.title(f":cup_with_straw: Pending Smoothie orders :cup_with_straw:")
+st.write("""Orders that need to be filled.""")
 
-
-# write directly to the app
-st.title(f"Orders Devlivered Check :cup_with_straw:")
-st.write("""Check the **Orders**""")
-
-session = get_session()
+session = get_active_session()
 my_dataframe = session.table("smoothies.public.orders").filter(col("ORDER_FILLED")==0).collect()
 
 if my_dataframe:
-  editable_df = st.data_editor(my_dataframe)
-  submitted = st.button('Submit')
-  if submitted:
-    st.success("Someone clicked the button.")
-    try:
-      og_dataset = session.table("smoothies.public.orders")
-      edited_dataset = session.create_dataframe(editable_df)
-      og_dataset.merge(edited_dataset
-                       , (og_dataset['ORDER_UID'] == edited_dataset['ORDER_UID'])
-                       , [when_mated().update({'ORDER_FILLED': edited_dataset['ORDER_FILLED']})]
-                      )
-      st.success("Order(s) Updated!")
-    except:
-      st.write('Something went wrong.')
-  
+    editable_df = st.data_editor(my_dataframe)
+    submitted = st.button('Submit')
+    if submitted:
+        
+        og_dataset = session.table("smoothies.public.orders")
+        edited_dataset = session.create_dataframe(editable_df)
+
+        try:
+            og_dataset.merge(edited_dataset
+                     , (og_dataset['ORDER_UID'] == edited_dataset['ORDER_UID'])
+                     , [when_matched().update({'ORDER_FILLED': edited_dataset['ORDER_FILLED']})]
+                    )
+            st.success('Someone clicked the button.',icon = '👍')
+        except:
+            st.write('Something went wrong.')
+
 else:
-  st.success('There are no pending orders right now')
-
-
-
-
-
-
-
-
-
-
-
-
-
-  
+    st.success('There are no pending orders right now',icon = '👍')
